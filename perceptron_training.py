f600059e1568db3f3de4058cbfcf4468d285df8c
Coopez <niklas.erd@gmail.com>
2020-11-23 17:19:02 +0100

    Code works and seems to be done

--- perceptron_training.py
+++ perceptron_training.py
@@ -11,39 +11,40 @@ import pandas as pd
 def gen_data(P,N):
     # P number of vectors, N dimensionality of vectors
     draw = np.random.normal(size= (P,N))
-    data = np.zeros((P,N+1))
+    data = np.ones((P,N+1))
     data[:,:-1] = draw
     for vector in range(0,P):
         if np.random.rand() >= 0.5:
-            data[vector,-1] = 1   
+            data[vector,-1] = -1   
     return data
 
 def loc_potential(w,e):
-    return np.dot(w,e[0:-2]*e[-1])
+    return np.dot(w,[number*e[-1] for number in  e[0:-1]])
 
 class Perceptron:
-    
     def __init__(self,size,n_data):
         self.P = n_data
         self.N = size
-        self.weights = [0 for i in range(0,self.N)] # init weights as 0
-        self.train_data = gen_data(self.P,self.N)
+        self.weights = np.zeros(self.N) # init weights as 0
+        self.train_data = gen_data(int(self.P),int(self.N))
     
     def rosenblatt(self,example):
-        if loc_potential(self.weights,self.train_data[example]) <= 0:
-            self.weights = self.weights + (self.train_data[example][0:-2]*self.train_data[example][-1])/self.N
-            return 1
-        return 0
+        if loc_potential(self.weights,self.train_data[example,:]) <= 0:
+            self.weights = self.weights + (self.train_data[example,0:-1]*self.train_data[example,-1])/self.N
+            return 0
+        return 1
     
     def training(self,epochs):
-        n = 0
-        while  n  < epochs:
-            E_sum = 0
+        n = 1
+        while  n  <= epochs:
+            E_sum = 1
             for example in range(0,self.P): # exersice says 1 to P, but since this is index, I would start at 0
-                E_sum += self.rosenblatt(example)           
+                E_sum += self.rosenblatt(example)    
+                      
                 if E_sum == self.P:
-                    return "success"
-        return "reached_nmax"
+                    return "success",n-1
+            n+=1
+        return "reached_nmax",n-1
 
 def main():
     #Experiments:
@@ -51,19 +52,26 @@ def main():
     nmax = 100
     N = 20
     alpha = [0.75+0.25*i for i in range(0,10)]
-    P = alpha *N
+    P = [int(a*N) for a in alpha]
     results = []
+    epochs = []
     for p in P: # do experiment for each P
         suc_sum = 0
         dt_set = 0
-        while dt_set < nD:
-            model = Perceptron(p,N)
-            if model.training(nmax)  == "success":
-               suc_sum +=1
+        e = 0
+        while dt_set < nD: 
+            #print(dt_set)
+            model = Perceptron(N,p)
+            train, epoch = model.training(nmax)
+            e += epoch 
+            if train  == "success":   
+                suc_sum +=1
             dt_set += 1
         results.append(suc_sum / nD)
-
-
+        epochs.append(e/nD)
+    print(results)
+    print(epochs)
 
 # Testing
 #gen_data(2,5)
+main()
