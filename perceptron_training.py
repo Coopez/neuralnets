22a6a77ddfc20d91382c60da971c0bfdaa803875
Coopez <niklas.erd@gmail.com>
2020-11-24 15:35:01 +0100

    added some comments and functionality

--- perceptron_training.py
+++ perceptron_training.py
@@ -1,15 +1,12 @@
 import numpy as np
-import math
-import pandas as pd
 
 # Code for assignment 1:
-#Rosenblatt Perceptron
-
-# Question - what imports would be dine, and what would not be fine
-# aka would numpy be fine here?
+# Rosenblatt Perceptron
+# Authors: Sebastian Prehn & Niklas Erdmann, 11.2020
 
 def gen_data(P,N):
     # P number of vectors, N dimensionality of vectors
+    # returns a matrix of data vectors with the last column being the labels 1,-1
     draw = np.random.normal(size= (P,N))
     data = np.ones((P,N+1))
     data[:,:-1] = draw
@@ -25,7 +22,7 @@ class Perceptron:
     def __init__(self,size,n_data):
         self.P = n_data
         self.N = size
-        self.weights = np.zeros(self.N) # init weights as 0
+        self.weights = np.zeros(self.N) # init weights as 0s
         self.train_data = gen_data(int(self.P),int(self.N))
     
     def rosenblatt(self,example):
@@ -38,29 +35,32 @@ class Perceptron:
         n = 1
         while  n  <= epochs:
             E_sum = 1
-            for example in range(0,self.P): # exersice says 1 to P, but since this is index, I would start at 0
-                E_sum += self.rosenblatt(example)    
-                      
+            for example in range(0,self.P): # exercise says 1 to P, but since this is an index, we start at 0 and go up to, but not including P 
+                E_sum += self.rosenblatt(example)       
                 if E_sum == self.P:
                     return "success",n-1
             n+=1
         return "reached_nmax",n-1
 
 def main():
-    #Experiments:
-    nD =  50 
-    nmax = 100
-    N = 20
+
+    #######################
+    ## Experiment Settings:
+    nD =  50  # Number of Experiment runs
+    nmax = 100 # Maximal Epochs per Experiment
+    N = 20 # size of each Sample instance
+    print_avgEpochs = True # if True will print out mean epochs to reach results for each P.
+    #######################
+
     alpha = [0.75+0.25*i for i in range(0,10)]
     P = [int(a*N) for a in alpha]
     results = []
     epochs = []
-    for p in P: # do experiment for each P
+    for p in P: # do experiment for each item in P
         suc_sum = 0
         dt_set = 0
         e = 0
         while dt_set < nD: 
-            #print(dt_set)
             model = Perceptron(N,p)
             train, epoch = model.training(nmax)
             e += epoch 
@@ -69,9 +69,8 @@ def main():
             dt_set += 1
         results.append(suc_sum / nD)
         epochs.append(e/nD)
-    print(results)
-    print(epochs)
+    print("Results: " + str(results))
+    if print_avgEpochs:
+        print("Average number of Epochs for each P: "+ str(epochs))
 
-# Testing
-#gen_data(2,5)
 main()
